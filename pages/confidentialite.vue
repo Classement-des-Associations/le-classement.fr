e8f7702921cd997dd4d01e6bf9af2b9bf77bc836
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-05-27 18:08:14 +0200

    fix #6

--- pages/confidentialite.vue
+++ pages/confidentialite.vue
@@ -1,3 +1,4 @@
 <template>
-  <ContentDoc path="privacy" tag="article" class="prose prose-zinc md:prose-lg lg:prose-xl mx-auto my-10 md:my-20" />
+  <ContentDoc path="privacy" tag="article"
+    class="px-4 prose prose-zinc md:prose-lg lg:prose-xl mx-auto my-10 md:my-20" />
 </template>
--- pages/reglement.vue
+++ pages/reglement.vue
@@ -1,3 +1,3 @@
 <template>
-  <ContentDoc path="rules" tag="article" class="prose prose-zinc md:prose-lg lg:prose-xl mx-auto my-10 md:my-20" />
+  <ContentDoc path="rules" tag="article" class="px-4 prose prose-zinc md:prose-lg lg:prose-xl mx-auto my-10 md:my-20" />
 </template>
