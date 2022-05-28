5eadf4ee0ef6ae2fe8d7b28e6b01007414678705
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-05-28 04:02:33 +0200

    create palmares for mobile

--- components/organisms/OrganismsWinners.vue
+++ components/organisms/OrganismsWinners.vue
@@ -14,67 +14,96 @@ const others = computed(() => props.winners.slice(3))
 
 <template>
   <div>
-    <ul class="grid grid-cols-3 gap-[4.25rem] justify-items-center uppercase  text-center">
-      <li class="mt-[4.75rem] px-[3.25rem] pt-11 w-full bg-white rounded-t-[1.25rem] shadow-default relative">
-        <div class="absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
-          <NuxtLink :to="podium[0].link" target="_blank">
-            <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
-          </NuxtLink>
-        </div>
-        <span class="text-[4rem] font-extrabold">2</span>
-        <div class="mt-6 text-lg space-y-4">
-          <h3 class="font-bold">
-            {{ podium[1].name }}
-          </h3>
-          <span>{{ podium[1].school }}</span>
-        </div>
-        <img src="/image.png" alt="" class="mt-11 rounded-t-[1.25rem]">
+    <ul class="grid grid-cols-3 gap-4 lg:gap-[4.25rem] justify-items-center uppercase  text-center">
+      <li class="mt-14 lg:mt-[4.75rem] w-full">
+        <NuxtLink :to="podium[1].link" target="_blank">
+          <div class="lg:hidden text-sm md:text-lg space-y-2">
+            <h3 class="font-bold">
+              {{ podium[1].name }}
+            </h3>
+            <div>{{ podium[1].school }}</div>
+          </div>
+          <div
+            class="mt-6 lg:mt-0 w-full h-full px-[3.25rem] pt-11 bg-white rounded-t-[1.25rem] shadow-default relative">
+            <div class="hidden md:block absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
+              <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
+            </div>
+            <span class="text-5xl lg:text-[4rem] font-extrabold">2</span>
+            <div class="hidden lg:block mt-6 lg:text-lg space-y-4">
+              <h3 class="font-bold">
+                {{ podium[1].name }}
+              </h3>
+              <div>{{ podium[1].school }}</div>
+            </div>
+            <img src="/image.png" alt="" class="hidden lg:block mt-11 rounded-t-[1.25rem]">
+          </div>
+        </NuxtLink>
       </li>
-      <li class="px-[3.25rem] pt-11 w-full bg-white rounded-t-[1.25rem] shadow-default relative">
-        <div class="absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
-          <NuxtLink :to="podium[0].link" target="_blank">
-            <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
-          </NuxtLink>
-        </div>
-        <span class="text-[4rem] font-extrabold">1</span>
-        <div class="mt-6 text-lg space-y-4">
-          <h3 class="font-bold">
-            {{ podium[0].name }}
-          </h3>
-          <span>{{ podium[0].school }}</span>
-        </div>
-        <img src="/image.png" alt="" class="mt-11 h-1/2 object-cover rounded-t-[1.25rem]">
+      <li class="w-full">
+        <NuxtLink :to="podium[0].link" target="_blank">
+          <div class="lg:hidden text-sm md:text-lg space-y-2">
+            <h3 class="font-bold">
+              {{ podium[0].name }}
+            </h3>
+            <div>{{ podium[0].school }}</div>
+          </div>
+          <div
+            class="mt-6 lg:mt-0 w-full h-full px-[3.25rem] pt-11 bg-white rounded-t-[1.25rem] shadow-default relative">
+            <div class="hidden md:block absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
+              <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
+            </div>
+            <div class="relative text-5xl lg:text-[4rem] font-extrabold">1
+              <img src="/medal.png" alt="Médaille en or"
+                class="absolute right-1/2 transform translate-x-[90%] top-1/2 lg:w-9 lg:h-9">
+            </div>
+            <div class="hidden lg:block mt-6 lg:text-lg space-y-4">
+              <h3 class="font-bold">
+                {{ podium[0].name }}
+              </h3>
+              <div>{{ podium[0].school }}</div>
+            </div>
+            <img src="/image.png" alt="" class="hidden lg:block mt-11 h-1/2 object-cover rounded-t-[1.25rem]">
+          </div>
+        </NuxtLink>
       </li>
-      <li class="mt-[9.125rem] px-[3.25rem] pt-11 w-full bg-white rounded-t-[1.25rem] shadow-default relative">
-        <div class="absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
-          <NuxtLink :to="podium[0].link" target="_blank">
-            <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
-          </NuxtLink>
-        </div>
-        <span class="text-[4rem] font-extrabold">3</span>
-        <div class="mt-6 text-lg space-y-4">
-          <h3 class="font-bold">
-            {{ podium[2].name }}
-          </h3>
-          <span>{{ podium[2].school }}</span>
-        </div>
-        <img src="/image.png" alt="" class="mt-11 object-cover rounded-t-[1.25rem]">
+      <li class="mt-12 lg:mt-[9.125rem] w-full">
+        <NuxtLink :to="podium[0].link" target="_blank">
+          <div class="lg:hidden text-sm md:text-lg space-y-2">
+            <h3 class="font-bold">
+              {{ podium[2].name }}
+            </h3>
+            <div>{{ podium[2].school }}</div>
+          </div>
+          <div class="mt-6 lg:mt-0 pb-3 px-[3.25rem] pt-11 w-full bg-white rounded-t-[1.25rem] shadow-default relative">
+            <div class="hidden md:block absolute top-4 right-4 p-3 bg-primary-variation-1 text-white rounded-full">
+              <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
+            </div>
+            <span class="text-5xl lg:text-[4rem] font-extrabold">3</span>
+            <div class="hidden lg:block mt-6 text-sm md:text-lg space-y-4">
+              <h3 class="font-bold">
+                {{ podium[2].name }}
+              </h3>
+              <div>{{ podium[2].school }}</div>
+            </div>
+            <img src="/image.png" alt="" class="hidden lg:block mt-11 object-cover rounded-t-[1.25rem]">
+          </div>
+        </NuxtLink>
       </li>
     </ul>
-    <ul class="relative w-full bg-white top-[-6rem] rounded-b-[1.125rem] divide-y divide-[#808080]/10">
-      <li v-for="(other, index) in others" class="py-7 flex flex-row justify-between items-center uppercase">
-        <div class="ml-9 flex flex-row items-center ">
-          <span class="text-[2.625rem] font-extrabold">{{ index + 3 }}</span>
-          <div class="ml-12 space-y-2">
-            <h3 class="text-lg font-bold">{{ other.name }}</h3>
-            <span class="text-[#808080] text-lg">{{ other.school }}</span>
+    <ul class="relative w-full bg-white lg:top-[-6rem] rounded-b-[1.125rem] divide-y divide-[#808080]/10">
+      <li v-for="(other, index) in others" class="py-7 uppercase">
+        <NuxtLink :to="podium[0].link" target="_blank" class="flex flex-row justify-between items-center">
+          <div class="ml-9 flex flex-row items-center ">
+            <span class="text-[1.75rem]  lg:text-[2.625rem] font-extrabold">{{ index + 3 }}</span>
+            <div class="ml-12 space-y-2">
+              <h3 class="text-sm lg:text-lg font-semibold lg:font-bold">{{ other.name }}</h3>
+              <span class="text-[#808080] text-sm lg:text-lg">{{ other.school }}</span>
+            </div>
           </div>
-        </div>
-        <div class="mr-4 p-3 bg-primary-variation-1 text-white rounded-full">
-          <NuxtLink :to="podium[0].link" target="_blank">
+          <div class="hidden md:block mr-4 p-3 bg-primary-variation-1 text-white rounded-full">
             <AtomsIconsGoTo /> <span class="sr-only">Vers le site de l'association</span>
-          </NuxtLink>
-        </div>
+          </div>
+        </NuxtLink>
       </li>
     </ul>
   </div>
