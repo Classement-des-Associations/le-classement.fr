ac2de98fe33b3a48b27e7ea60998adc7f3989cff
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-06-17 21:54:36 +0200

    fix: update plugin for clarity

--- plugins/clarity.client.ts
+++ plugins/clarity.client.ts
@@ -9,6 +9,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   const script = document.createElement('script')
   script.type = 'text/javascript'
   script.innerHTML = `
-  (function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${microsoftClarityID});`
+  (function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "${microsoftClarityID}");`
   document.head.appendChild(script)
 })
