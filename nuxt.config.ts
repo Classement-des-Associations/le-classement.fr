2e782304eea01c9f42223eeba386b315eef64177
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-08-08 19:48:07 +0200

    build: update to prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
+  },
   app: {
     head: {
       meta: [
