c30f4f37cd6aa4dac21bace793df6b5cc77784db
Barbapapazes <e.soubiran25@gmail.com>
2022-11-18 19:49:10 +0100

    feat: create links tree

---
+++ components/content/links/LinksListSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h2 class="text-2xl text-black font-bold">
+      <ContentSlot :use="$slots.default"></ContentSlot>
+    </h2>
+    <ul class="mt-4">
+      <ContentSlot :use="$slots.items"></ContentSlot>
+    </ul>
+  </section>
+</template>
