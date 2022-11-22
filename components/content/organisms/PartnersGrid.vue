1c7480e8f221d0f03180a4fda940906f1e6ce718
Barbapapazes <e.soubiran25@gmail.com>
2022-11-22 18:12:25 +0100

    fix: overwrite nuxt link props

--- components/content/organisms/PartnersGrid.vue
+++ components/content/organisms/PartnersGrid.vue
@@ -17,7 +17,7 @@ defineProps<{
     </AtomsSectionTitle>
     <ul class="mt-11 grid grid-cols-2 md:grid-cols-5 items-center justify-center">
       <li v-for="partner in partners" :ley="partner.src">
-        <NuxtLink :to="partner.href">
+        <NuxtLink :to="partner.href" target="_blank" rel="noopener">
           <div class="aspect-w-square aspect-h-square">
             <img loading="lazy" :src="partner.src" :alt="partner.alt" class=" object-contain">
           </div>
