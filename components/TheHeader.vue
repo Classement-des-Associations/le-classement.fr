3d8129f8a902084df7855c1b4b3eff3559cfb417
Barbapapazes <e.soubiran25@gmail.com>
2022-12-05 15:46:21 +0100

    feat: right click on header logo to discover design kit

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -7,7 +7,7 @@ const { navigation } = useContent()
   <header class="bg-white p-4">
     <div class="max-w-screen-xl mx-auto flex flex-row justify-between">
       <div class="flex flex-row space-x-8 justify-center items-center">
-        <NuxtLink to="/">
+        <NuxtLink to="/" @click.right="navigateTo('design-kit')">
           <AtomsLogosClassementShort
             class="h-[40px] w-[40px] md:h-[50px] md:w-[50px] text-black hover:text-primary-base motion-safe:transition-all motion-safe:duration-300" />
         </NuxtLink>
