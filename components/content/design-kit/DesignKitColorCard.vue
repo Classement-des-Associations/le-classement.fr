4f7ef669956908f3cfeb9ebec2e88b0f56e24ebf
Barbapapazes <esteban.soubiran@insa-cvl.fr>
2022-12-07 11:04:06 +0100

    feat: design kit (#56)
    * feat: create design kit page
    
    * feat: right click on header logo to discover design kit
    
    * fix: prevent right click
    
    * feat: add logos
    
    * feat: add content
    
    * fix: typo
    
    * feat: add typography
    
    * fix: props
    
    * fix: grey to zinc
    
    * fix: border

---
+++ components/content/design-kit/DesignKitColorCard.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const { hexaColor } = defineProps<{
+  colorClass: string,
+  hexaColor: string
+}>()
+
+const { copy } = useClipboard({ source: hexaColor })
+</script>
+
+<template>
+  <li>
+    <div class="overflow-hidden rounded-xl h-[140px]" :class="colorClass"></div>
+    <div>
+      <div class="flex justify-between items-center">
+        <p class="font-semibold text-black" v-if="$slots.color">
+          <ContentSlot :use="$slots.color" unwrap="p"> </ContentSlot>
+        </p>
+        <button @click="copy()" type="button"
+          class="font-medium focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 focus:ring-offset-white text-sm px-4 py-2 border border-transparent text-zinc-500 hover:text-zinc-700 focus:text-zinc-700 disabled:hover:text-zinc-500 inline-flex items-center rounded-lg"
+          v-if="$slots.buttonText">
+          <ContentSlot :use="$slots.buttonText" unwrap="p"></ContentSlot>
+        </button>
+      </div>
+      <p class="text-zinc-500">
+        {{ hexaColor }}
+      </p>
+    </div>
+  </li>
+</template>
