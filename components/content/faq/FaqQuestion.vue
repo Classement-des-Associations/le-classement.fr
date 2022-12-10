041e52a81e3f324e4e04d2657f7ea7d686d3bd7e
Barbapapazes <e.soubiran25@gmail.com>
2022-12-10 16:22:42 +0100

    refactor: faq

---
+++ components/content/faq/FaqQuestion.vue
@@ -0,0 +1,9 @@
+<template>
+  <li>
+    <h3>
+      <ContentSlot :use="$slots.question" unwrap="p" />
+    </h3>
+    <!-- il faut utiliser un v-show pour que le contenu soit quand même sur la page -->
+    <ContentSlot :use="$slots.answer" />
+  </li>
+</template>
---
+++ components/content/faq/FaqSection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+  sectionClass?: string
+  id?: string
+}>()
+</script>
+
+<template>
+  <section :class="sectionClass" :id="id">
+    <h2>
+      <ContentSlot :use="$slots.title" unwrap="p" />
+    </h2>
+    <ol>
+      <ContentSlot :use="$slots.questions" />
+    </ol>
+  </section>
+</template>
