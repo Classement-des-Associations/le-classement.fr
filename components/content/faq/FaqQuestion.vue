e857d169c0e75682925c2d62126d5a78a080b181
Barbapapazes <e.soubiran25@gmail.com>
2022-12-12 10:56:10 +0100

    fix: faq

--- components/content/faq/FaqQuestion.vue
+++ components/content/faq/FaqQuestion.vue
@@ -1,9 +1,18 @@
+<script lang="ts" setup>
+const open = ref(false)
+const icon = computed(() => open.value ? 'heroicons:chevron-up-solid' : 'heroicons:chevron-down-solid')
+</script>
+
 <template>
-  <li>
-    <h3>
-      <ContentSlot :use="$slots.question" unwrap="p" />
+  <li class="pb-4 border-b border-zinc-200">
+    <h3 class="text-lg font-medium text-black">
+      <button @click="open = !open" class="w-full flex flex-row justify-between items-center">
+        <ContentSlot :use="$slots.question" unwrap="p" />
+        <Icon :name="icon" class="w-4 h-4" />
+      </button>
     </h3>
-    <!-- il faut utiliser un v-show pour que le contenu soit quand même sur la page -->
-    <ContentSlot :use="$slots.answer" />
+    <div v-show="open" class="mt-2 prose max-w-none">
+      <ContentSlot :use="$slots.answer" />
+    </div>
   </li>
 </template>
--- components/content/faq/FaqSection.vue
+++ components/content/faq/FaqSection.vue
@@ -6,11 +6,11 @@ defineProps<{
 </script>
 
 <template>
-  <section :class="sectionClass" :id="id">
-    <h2>
+  <section class="mx-auto sm:px-6 lg:px-8 px-4 max-w-7xl" :class="sectionClass" :id="id">
+    <h2 class="pb-6 text-2xl font-semibold text-black">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </h2>
-    <ol>
+    <ol class="flex flex-col gap-8">
       <ContentSlot :use="$slots.questions" />
     </ol>
   </section>
