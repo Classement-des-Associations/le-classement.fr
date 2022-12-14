d2018c03bd87cbfe036484866593254333b6844c
Barbapapazes <e.soubiran25@gmail.com>
2022-12-14 16:27:26 +0100

    fix: add preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,11 @@ export default defineNuxtConfig({
           content: "summary_large_image",
         },
         {
+          hid: "og:image",
+          property: "og:image",
+          content: "https://le-classement.fr/preview.png",
+        },
+        {
           hid: "og:image:alt",
           property: "og:image:alt",
           content: "Image d'illustration pour le classement",
