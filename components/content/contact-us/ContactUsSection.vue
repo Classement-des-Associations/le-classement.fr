b3433e9bc1d064881fa56939fcb63457f0b9720d
Barbapapazes <e.soubiran25@gmail.com>
2022-12-16 21:06:17 +0100

    fix: background size

--- components/content/contact-us/ContactUsSection.vue
+++ components/content/contact-us/ContactUsSection.vue
@@ -5,12 +5,14 @@ defineProps<{
 </script>
 
 <template>
-  <BaseSection :class="sectionClass" class="bg-ultra-light-grey py-14">
-    <h1 class="sr-only">
-      <ContentSlot :use="$slots.srOnly" unwrap="p"></ContentSlot>
-    </h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-24">
-      <ContentSlot :use="$slots.content" unwrap="p"></ContentSlot>
-    </div>
-  </BaseSection>
+  <div class="bg-ultra-light-grey py-14">
+    <BaseSection :class="sectionClass">
+      <h1 class="sr-only">
+        <ContentSlot :use="$slots.srOnly" unwrap="p"></ContentSlot>
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-24">
+        <ContentSlot :use="$slots.content" unwrap="p"></ContentSlot>
+      </div>
+    </BaseSection>
+  </div>
 </template>
