cf7579700f71d65b807ff9a7c55fd345724a7da1
Barbapapazes <e.soubiran25@gmail.com>
2022-12-27 17:05:53 +0100

    fix: title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,18 +40,6 @@ export default defineNuxtConfig({
           name: "google-site-verification",
           content: "TdbVQQq00musVnOqyiFS2ulrIvi29dUNAhPXtGXWags",
         },
-        {
-          hid: "og:site_name",
-          property: "og:site_name",
-          content: "Le Classement des Associations",
-        },
-        { hid: "og:type", property: "og:type", content: "website" },
-        { hid: "twitter:site", name: "twitter:site", content: "@Leclassement" },
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
       ],
     },
   },
