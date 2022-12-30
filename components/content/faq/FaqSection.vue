035b4fb9dac88de928bd9bbd0eb4aa7a0a23eebc
Barbapapazes <e.soubiran25@gmail.com>
2022-12-30 17:43:53 +0100

    fix: update questions

--- components/content/faq/FaqSection.vue
+++ components/content/faq/FaqSection.vue
@@ -6,12 +6,12 @@ defineProps<{
 </script>
 
 <template>
-  <section class="mx-auto sm:px-6 lg:px-8 px-4 max-w-7xl" :class="sectionClass" :id="id">
+  <BaseSection :class="sectionClass" :id="id">
     <h2 class="pb-6 text-2xl font-semibold text-black">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </h2>
     <ol class="flex flex-col gap-8">
       <ContentSlot :use="$slots.questions" />
     </ol>
-  </section>
+  </BaseSection>
 </template>
