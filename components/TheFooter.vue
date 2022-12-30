2c77269e448487bb4ee56fe35f12244f0ea157cd
Barbapapazes <e.soubiran25@gmail.com>
2022-12-30 17:57:48 +0100

    fix: header and footer section

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,8 +4,8 @@ const { navigation } = useContent()
 </script>
 
 <template>
-  <footer class="bg-black px-4 py-11">
-    <div class="max-w-screen-xl mx-auto flex flex-col gap-14">
+  <div class="bg-black">
+    <BaseSection :is="'footer'" class="py-11 flex flex-col gap-14">
       <div class="flex flex-col gap-11 lg:gap-0 lg:flex-row lg:justify-between lg:items-center">
         <div class="contents lg:flex lg:flex-row lg:items-center">
           <AtomsLogosClassementFull class="mx-auto" @click.right.prevent="navigateTo('design-kit')" />
@@ -21,6 +21,7 @@ const { navigation } = useContent()
             </template>
           </ul>
         </div>
+
         <nav aria-labelledby="secondary-nav">
           <span id="secondary-nav" class="sr-only">Navigation secondaire</span>
           <ul
@@ -34,6 +35,7 @@ const { navigation } = useContent()
           </ul>
         </nav>
       </div>
+
       <div class="flex flex-col items-center gap-4">
         <div class="h-[1px] w-full max-w-[256px] rounded-full bg-white"></div>
         <p class="flex flex-col md:flex-row items-center gap-2 md:gap-0 text-white text-sm font-light"><span>Le
@@ -41,6 +43,6 @@ const { navigation } = useContent()
             <NuxtLink to="https://aneo.eu" rel="noopener" target="_blank">Par Aneo</NuxtLink>
           </span></p>
       </div>
-    </div>
-  </footer>
+    </BaseSection>
+  </div>
 </template>
