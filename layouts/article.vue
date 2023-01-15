5426594b1a279e40b2546ef46db05a039306deb8
Barbapapazes <e.soubiran25@gmail.com>
2023-01-15 01:41:10 +0100

    fix: responsive

--- layouts/article.vue
+++ layouts/article.vue
@@ -41,23 +41,23 @@ const proseClass = function (part: Part = 'classement') {
     <BlogToc class="hidden lg:block fixed right-8 top-1/2 transform -translate-y-1/2 z-20"></BlogToc>
 
     <article class="max-w-4xl mx-auto px-4 flex flex-col">
-      <h1 class="text-4xl md:text-5xl font-bold tracking-tight bg-clip-text text-transparent"
+      <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight bg-clip-text text-transparent"
         :class="colors.backgroundGradient">
         {{ page.title }}
       </h1>
-      <figure class="mt-8">
+      <figure class="mt-4 md:mt-8">
         <img v-if="page.image" :src="page.image.src" :alt="page.image.alt" class="rounded-2xl" loading="lazy">
-        <figcaption class="mt-2 flex flex-row text-black font-light">
+        <figcaption class="mt-1 md:mt-2 text-sm md:text-base flex flex-row text-black font-light">
           <p>
             {{ page.image.alt }}
+            <span> </span>
+            <time :datetime="datetime">
+              Publié le {{ formattedDate }}.
+            </time>
           </p>
-          &nbsp;
-          <time :datetime="datetime">
-            Publié le {{ formattedDate }}.
-          </time>
         </figcaption>
       </figure>
-      <div class="mt-6 w-full mx-auto prose max-w-2xl prose-zinc prose-img:rounded-lg
+      <div class="mt-2 md:mt-6 w-full mx-auto prose max-w-2xl prose-zinc prose-img:rounded-lg
         prose-a:prose-headings:no-underline prose-a:prose-headings:font-semibold
         prose-a:transition-all prose-a:ease-in  prose-a:prose-p:font-semibold prose-a:prose-p:bg-clip-text prose-a:prose-p:text-transparent
         " :class="proseClass(page.type)">
