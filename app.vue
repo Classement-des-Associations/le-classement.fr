92b472c6bee9c8a26727301a0307dea9e911a7d9
Barbapapazes <e.soubiran25@gmail.com>
2023-01-23 16:40:08 +0100

    feat: add page transition

--- app.vue
+++ app.vue
@@ -29,3 +29,24 @@ const htmlClass = computed(() => {
     </Body>
   </Html>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.3rem);
+}
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.2s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  opacity: 0;
+  filter: blur(0.3rem);
+}
+</style>
