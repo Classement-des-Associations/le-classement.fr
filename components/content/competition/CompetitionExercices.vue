76ad015465d3657f8a2a4a1e3cd6512db18cc0a1
Barbapapazes <e.soubiran25@gmail.com>
2023-02-01 06:46:49 +0100

    fix: concours

--- components/content/competition/CompetitionExercices.vue
+++ components/content/competition/CompetitionExercices.vue
@@ -8,8 +8,8 @@ const { data: exercices } = await useConcoursExercices()
 
 <template>
   <div v-if="exercices" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-8 md:gap-10">
-    <article v-for="item in exercices.body" :key="item.title">
-      <NuxtLink :to="item.to">
+    <article v-for="item in exercices.body" :key="item.title" class="h-full">
+      <NuxtLink :to="item.to" class="h-full">
         <HomeItem :tag="tag" :icon="item.icon">
           <template #title>
             {{ item.title }}
