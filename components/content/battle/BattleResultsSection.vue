e06d6a89438fad05d71297a849e18b0ae17fcd52
Barbapapazes <e.soubiran25@gmail.com>
2023-02-15 15:15:26 +0100

    fix: winners

--- components/content/battle/BattleResultsSection.vue
+++ components/content/battle/BattleResultsSection.vue
@@ -5,33 +5,46 @@ const props = defineProps<{
 }>()
 
 const { data: duels } = await useDuels(props.duels)
+
+const calculatePercentage = (value: number, total: number) => {
+  const result = (value / total) * 100
+  if (Number.isNaN(result)) {
+    return 0
+  }
+
+  return Math.round((value / total) * 100)
+}
+
+const isWinner = (duel: any, player: 'player1' | 'player2') => {
+  if (player === 'player1') {
+    return duel.likesPlayer1 > duel.likesPlayer2
+  } else if (player === 'player2') {
+    return duel.likesPlayer2 > duel.likesPlayer1
+  }
+}
 </script>
 
 <template>
   <BaseSection :class="sectionClass">
-    <Prose v-if="duels" class="mx-auto max-w-none prose-td:align-middle">
-      <h2>
-        <ContentSlot :use="$slots.title" unwrap="p" />
-      </h2>
-      <table>
+    <Prose v-if="duels" class="mx-auto max-w-none prose-td:align-middle prose-th:text-lg prose-th:py-4 prose-th:px-8 prose-td:py-4 prose-td:px-8">
+      <table class="w-auto mx-auto">
         <thead>
           <tr>
-            <th>Duel</th>
-            <th>Association 1</th>
-            <th>Résultat</th>
+            <th>
+              <div class="flex flex-row justify-end">
+                Association 1
+              </div>
+            </th>
+            <th class="flex flex-row justify-center">
+              Résultat (votes - %)
+            </th>
             <th>Association 2</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="duel in duels.body" :key="duel.id">
             <td>
-              <span class="flex flex-row items-center">
-                {{ duel.id }}
-              </span>
-            </td>
-            <!-- Add bold to winner -->
-            <td>
-              <div class="flex flex-row items-center gap-2">
+              <div class="flex flex-row justify-end items-center gap-2" :class="{'font-bold': isWinner(duel, 'player1')}">
                 <div class="not-prose">
                   <img :src="duel.player1.image.src" :alt="duel.player1.image.alt" class="h-6 w-auto object-cover">
                 </div>
@@ -41,20 +54,24 @@ const { data: duels } = await useDuels(props.duels)
               </div>
             </td>
             <td>
-              <dl class="flex flex-row gap-1 items-center">
+              <dl class="flex flex-row gap-1 items-center justify-center">
                 <dt class="sr-only">
                   Nombre de votes pour l'association {{ duel.player1.name }}
                 </dt>
-                <dd>{{ duel.likesPlayer1 }}</dd>
+                <dd class="tabular-nums" :class="{'font-bold': isWinner(duel, 'player1')}">
+                  {{ duel.likesPlayer1 }} ({{ calculatePercentage(duel.likesPlayer1, (duel.likesPlayer1 + duel.likesPlayer2)) }}%)
+                </dd>
                 <Icon name="fluent-emoji:high-voltage" class="h-5 w-5" />
                 <dt class="sr-only">
                   Nombre de votes pour l'association {{ duel.player2.name }}
                 </dt>
-                <dd>{{ duel.likesPlayer2 }}</dd>
+                <dd class="tabular-nums" :class="{'font-bold': isWinner(duel, 'player2')}">
+                  {{ duel.likesPlayer2 }} ({{ calculatePercentage(duel.likesPlayer2, (duel.likesPlayer1 + duel.likesPlayer2)) }}%)
+                </dd>
               </dl>
             </td>
             <td>
-              <div class="flex flex-row items-center gap-2">
+              <div class="flex flex-row items-center gap-2" :class="{'font-bold': isWinner(duel, 'player2')}">
                 <span>
                   {{ duel.player2.name }}
                 </span>
