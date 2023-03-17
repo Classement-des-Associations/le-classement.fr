accd699d47f6d692a8726ad2c141a88e22a0c63d
RICHARD Jeremy <jeremy.richard@viveris.fr>
2023-03-17 14:12:37 +0100

    refactor: :art: Three js optimisation and lights

--- components/content/home/HomeTrophy.vue
+++ components/content/home/HomeTrophy.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="gemWrapper" ref="gemWrapper" :style="{ opacity: ready ? 1 : 0 }" class="hidden sm:block transition duration-1000">
-    <div ref="gemAnim" />
+    <canvas ref="gemAnim" id="ThreeCanvas" />
   </div>
 </template>
 
@@ -10,6 +10,7 @@ import { useEventListener } from '@vueuse/core'
 const ready = ref()
 const gemWrapper = ref(null)
 const gemAnim = ref(null)
+let AnimationID
 
 if (process.client) {
   async function loadGem () {
@@ -20,87 +21,100 @@ if (process.client) {
 
     // Loaders
     const gltfLoader = new GLTFLoader()
-
-    // Renderer
-    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
-    renderer.outputEncoding = THREE.sRGBEncoding
-    renderer.toneMapping = THREE.ACESFilmicToneMapping
-    renderer.toneMappingExposure = 1
-    if (window.matchMedia('(min-width: 768px)').matches) {
-      renderer.setSize(400, 400)
-    } else if (window.matchMedia('(min-width: 640px)').matches) {
-      renderer.setSize(300, 300)
-    } else {
-      renderer.setSize(180, 180)
-    }
-
-    // Scene
+    // Three init
+    const canvas = document.getElementById('ThreeCanvas')
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true, canvas: canvas })
     const scene = new THREE.Scene()
-
     const pmremGenerator = new THREE.PMREMGenerator(renderer)
-    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
+    const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000)
+    const controls = new OrbitControls(camera, renderer.domElement)
+    const clock = new THREE.Clock()
 
-    // Gem
     let gem
 
-    gltfLoader.load('/assets/home/trophy.glb', function (gltf) {
-      gem = gltf.scene.children[0]
-      gem.scale.set(1.2, 1.2, 1.2)
-      gem.position.set(0, 0, 0)
-      scene.add(gem)
+    function init() {
+      // Renderer
+      renderer.setPixelRatio(window.innerWidth / window.innerHeight)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      renderer.toneMapping = THREE.ACESFilmicToneMapping
+      renderer.toneMappingExposure = 1
+      setRendererSize()
 
-      ready.value = true
-    })
+      // Scene
+      scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
 
-    // Renderer
-    gemAnim.value.appendChild(renderer.domElement)
-    renderer.setPixelRatio(window.devicePixelRatio)
-    function onWindowResize () {
+      // Camera
+      camera.aspect = 1
+      camera.position.z = 14
+
+      // Lights arround
+      let pos = [[5, -5, 5], [5, -5, -5], [-5, -5, 5], [-5, -5, -5]]
+      let lights = new THREE.Group()
+      for (let i = 0; i < 4; i++) {
+        let light = new THREE.PointLight(0xffffff, 1, 1000)
+        light.position.set(pos[i][0], pos[i][1], pos[i][2])
+        lights.add(light)
+      }
+      scene.add(lights)
+
+      // Controls
+      controls.enableDamping = true
+      controls.dampingFactor = 0.05
+      controls.enableZoom = false
+      controls.enablePan = false
+      controls.maxPolarAngle = Math.PI * 0.5
+      controls.minPolarAngle = Math.PI * 0.5
+
+      // Gem
+      gltfLoader.load('/assets/home/trophy.glb',
+        (gltf) => {
+          gem = gltf.scene
+          gem.scale.set(1.2, 1.2, 1.2)
+          gem.position.set(0, 0, 0)
+          scene.add(gem)
+          ready.value = true
+        }
+      )
+      useEventListener(window, 'resize', () => {onWindowResize()})
+    }
+
+    function setRendererSize() {
       if (window.matchMedia('(min-width: 768px)').matches) {
-        renderer.setSize(400, 400)
+      renderer.setSize(400, 400)
       } else if (window.matchMedia('(min-width: 640px)').matches) {
         renderer.setSize(300, 300)
       } else {
         renderer.setSize(180, 180)
       }
     }
-
-    useEventListener(window, 'resize', () => {
-      onWindowResize()
-    })
-
-    // Transition on load
-    gemWrapper.value.style.opacity = 0
-    setTimeout(() => {
-      gemWrapper.value.style.opacity = 1
-    }, 1000)
-
-    // Camera
-    const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000)
-    camera.aspect = 1
-    camera.position.z = 14
-
-    // Controls
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true
-    controls.dampingFactor = 0.05
-    controls.enableZoom = false
-    controls.enablePan = false
-    controls.maxPolarAngle = Math.PI * 0.5
-    controls.minPolarAngle = Math.PI * 0.5
-
-    controls.update()
+    
+    function onWindowResize () {
+      setRendererSize()
+      renderer.pixelRatio = window.innerWidth / window.innerHeight
+    }
 
     function animate () {
-      requestAnimationFrame(animate)
-      if (gem) {
-        gem.rotation.y -= 0.01
-      }
+      AnimationID = requestAnimationFrame(animate)
+      render()
+    }
+
+    function render () {
+      const delta = clock.getDelta()
       controls.update()
+      if (gem) {
+        gem.rotation.y -= delta * 0.4
+      }
       renderer.render(scene, camera)
     }
 
-    animate()
+    init()
+    AnimationID = requestAnimationFrame(animate)
+
+    // Transition on load
+    gemWrapper.value.style.opacity = 0
+      setTimeout(() => {
+        gemWrapper.value.style.opacity = 1
+      }, 1000)    
   }
 
   onMounted(() => requestIdleCallback(() => {
@@ -114,5 +128,9 @@ if (process.client) {
     }, { threshold: 0.5 })
     observer.observe(gemAnim.value)
   }))
+
+  onUnmounted(() => {
+    cancelAnimationFrame(AnimationID)
+  })
 }
 </script>
