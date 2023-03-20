6eb71204afca9a13b15c4d0b089925a0ece3ebcc
Barbapapazes <e.soubiran25@gmail.com>
2023-03-20 09:26:42 +0100

    fix: typo

--- components/content/ProseSection.vue
+++ components/content/ProseSection.vue
@@ -3,7 +3,7 @@ defineProps<{
   sectionClass?: string
 }>()
 
-const proseClass = 'prose prose-zinc prose-img:rounded-lg prose-a:prose-headings:no-underline prose-a:prose-headings:font-semibold prose-table:block prose-table:overflow-y-scroll'
+const proseClass = useProse()
 </script>
 
 <template>
