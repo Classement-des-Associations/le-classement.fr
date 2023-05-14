605d27c0c3aaee900a85c1fa23d238269f3d6ca6
Barbapapazes <e.soubiran25@gmail.com>
2023-05-14 18:42:46 +0200

    feat: sponsors home

--- components/content/home/HomeSponsors.vue
+++ components/content/home/HomeSponsors.vue
@@ -4,6 +4,7 @@ defineProps<{
   sponsors: {
     name: string;
     src: string;
+    class: string;
     width: string | number;
     height: string | number;
   }[]
@@ -12,21 +13,23 @@ defineProps<{
 
 <template>
   <BaseSection :class="sectionClass">
-    <h2 class="text-sm sm:text-base text-zinc-500 tracking-tight font-medium">
-      <ContentSlot :use="$slots.title" unwrap="p" />
-    </h2>
-    <ul class="mt-5">
-      <template v-for="sponsor in sponsors" :key="sponsor.name">
-        <li class="h-8 sm:h-11">
-          <img
-            :src="sponsor.src"
-            :alt="`Logo du sponsor ${sponsor.name}`"
-            class="h-full"
-            :width="sponsor.width"
-            :height="sponsor.height"
-          >
-        </li>
-      </template>
-    </ul>
+    <NuxtLink to="/partenaires">
+      <h2 class="text-sm sm:text-base text-zinc-500 tracking-tight font-medium">
+        <ContentSlot :use="$slots.title" unwrap="p" />
+      </h2>
+      <ul class="mt-5 flex flex-row gap-4">
+        <template v-for="sponsor in sponsors" :key="sponsor.name">
+          <li class="h-8 sm:h-11 flex flex-row items-center justify-center">
+            <img
+              :src="sponsor.src"
+              :alt="`Logo du sponsor ${sponsor.name}`"
+              :class="sponsor.class"
+              :width="sponsor.width"
+              :height="sponsor.height"
+            >
+          </li>
+        </template>
+      </ul>
+    </NuxtLink>
   </BaseSection>
 </template>
